427b9c46baea14828c4ef77a28f5554f3f882ba8
mohammadmehdisharife <mohammadmehdisharife@gmail.com>
2025-12-24 17:58:48 +0000

    add -Oz flag for low binrary size

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@ rm -rf build
 mkdir build
 
 # linux
-gcc csxl.c function.c memory.c log.c -o ./build/csxl
+gcc -Oz csxl.c function.c memory.c log.c -o ./build/csxl
 
 # win64
-x86_64-w64-mingw32-gcc csxl.c function.c memory.c log.c -o ./build/csxl_win64.exe
+x86_64-w64-mingw32-gcc -Oz csxl.c function.c memory.c log.c -o ./build/csxl_win64.exe
 
 # win32
-i686-w64-mingw32-gcc csxl.c function.c memory.c log.c -o ./build/csxl_win32.exe
+i686-w64-mingw32-gcc -Oz csxl.c function.c memory.c log.c -o ./build/csxl_win32.exe
