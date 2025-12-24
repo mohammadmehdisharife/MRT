ecee528905cfd83eab874bf9c614d24808f94737
mohammadmehdisharife <mohammadmehdisharife@gmail.com>
2025-12-24 18:17:32 +0000

    add auto make comperes arshive to build.sh

--- build.sh
+++ build.sh
@@ -9,3 +9,12 @@ x86_64-w64-mingw32-gcc -Oz csxl.c function.c memory.c log.c -o ./build/csxl_win6
 
 # win32
 i686-w64-mingw32-gcc -Oz csxl.c function.c memory.c log.c -o ./build/csxl_win32.exe
+
+# create archive for release
+zip -r csxl.zip build/
+tar -cf csxl.tar build/
+tar -czf csxl.tar.gz build/
+tar -cjf csxl.tar.bz2 build/
+tar -cJf csxl.tar.xz build/
+rar a csxl.rar build/
+7z a csxl.7z build/
